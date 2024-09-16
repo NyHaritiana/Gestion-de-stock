2df2cf04b3965961b41cecdbfacadc558d318cc8
NyHaritiana <ainaramandimby@gmail.com>
2024-09-16 12:36:12 +0200

    home cha

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,10 +1,31 @@
 import React, { useState } from "react";
+import Navbar from "./Navbar";
+import "../App.css";
+import materiel from "../assets/materiel.jpg";
 
 function Home() {
+  const [datatitle, setDatatitle] = useState([
+    { id: 1, libelle: "Matériels" },
+    { id: 2, libelle: "Clés" },
+    { id: 3, libelle: "Accessoires" },
+    { id: 4, libelle: "Équipements" },
+    { id: 5, libelle: "Fournitures" },
+  ]);
+  const [datafirstarticle, setDataarticle] = useState([
+    { id: 1, title: "Matériels", image: {materiel} },
+    { id: 2, title: "Clés", image: {materiel} },
+    { id: 3, title: "Accessoires", image: {materiel} },
+    { id: 1, title: "Équipements", image: {materiel} },
+  ]);
+  const [datasecondarticle, setDatasecondarticle] = useState([
+    { id: 1, title: "Équipements" },
+    { id: 2, title: "Fournitures" },
+    { id: 3, title: "Autres" },
+  ]);
   const [datahead, setDatahead] = useState([
     { id: 1, libelle: "Designation" },
     { id: 2, libelle: "Catégorie" },
-    { id: 3, libelle: "Quatiité" },
+    { id: 3, libelle: "Quatité" },
     { id: 4, libelle: "Unité" },
     { id: 5, libelle: "N° Comptable" },
     { id: 6, libelle: "Ref. Facture" },
@@ -12,46 +33,7 @@ function Home() {
   return (
     <>
       {/* ---------navbar------------ */}
-      <nav className="w-full py-4 bg-blue-800 shadow">
-        <div className="w-full container mx-auto flex flex-wrap items-center justify-between">
-          <nav>
-            <ul className="flex items-center justify-between font-bold text-sm text-white uppercase no-underline">
-              <li>
-                <a className="pr-6" href="#">
-                  <i className="fas fa-home"></i>
-                </a>
-              </li>
-              <li>
-                <a
-                  className="hover:text-gray-200 hover:underline px-4"
-                  href="#"
-                >
-                  Entrée
-                </a>
-              </li>
-              <li>
-                <a
-                  className="hover:text-gray-200 hover:underline px-4"
-                  href="#"
-                >
-                  Sortie
-                </a>
-              </li>
-            </ul>
-          </nav>
-          <div className="flex items-center text-lg no-underline text-white pr-6">
-            <a className="" href="#">
-              <i className="fab fa-facebook"></i>
-            </a>
-            <a className="pl-6" href="#">
-              <i className="fab fa-instagram"></i>
-            </a>
-            <a className="pl-6" href="#">
-              <i className="fab fa-twitter"></i>
-            </a>
-          </div>
-        </div>
-      </nav>
+      <Navbar />
       {/* -----------title----------- */}
       <header className="w-full container mx-auto">
         <div className="flex flex-col items-center py-12">
@@ -70,185 +52,91 @@ function Home() {
       <nav className="w-full py-4 border-t border-b bg-gray-100">
         <div className="w-full flex-grow sm:flex sm:items-center sm:w-auto">
           <div className="w-full container mx-auto flex flex-col sm:flex-row items-center justify-center text-sm font-bold uppercase mt-0 px-6 py-2">
-            <a href="#" className="hover:bg-gray-400 rounded py-2 px-4 mx-2">
-              Matériels
-            </a>
-            <a href="#" className="hover:bg-gray-400 rounded py-2 px-4 mx-2">
-              Clés
-            </a>
-            <a href="#" className="hover:bg-gray-400 rounded py-2 px-4 mx-2">
-              Accessoires
-            </a>
-            <a href="#" className="hover:bg-gray-400 rounded py-2 px-4 mx-2">
-              Équipements
-            </a>
-            <a href="#" className="hover:bg-gray-400 rounded py-2 px-4 mx-2">
-              Fournitures
-            </a>
+            <div className="scroll-container">
+              {datatitle.map((item) => (
+                <a
+                  href="#"
+                  className="scrollh hover:bg-gray-400 rounded py-2 px-4 mx-2"
+                  key={item.id}
+                >
+                  {item.libelle}
+                </a>
+              ))}
+            </div>
           </div>
         </div>
       </nav>
 
       <div className="container mx-auto flex flex-wrap py-6">
-        <section className="w-full flex flex-col px-3">
-          <article className="flex flex-col shadow my-4">
-            <a href="#" className="hover:opacity-75"></a>
-            <div className="bg-white flex flex-col justify-start p-6">
-              <a
-                href="#"
-                className="text-blue-700 text-sm font-bold uppercase pb-4"
-              >
-                Matériels
-              </a>
-              <table className="min-w-full divide-y divide-gray-200 table-fixed dark:divide-gray-700">
-                <thead className="bg-gray-100 dark:bg-gray-700">
-                  <tr>
-                    {datahead.map((item) => (
-                      <th
-                        scope="col"
-                        className="py-3 px-6 text-xs font-medium tracking-wider text-center text-gray-700 uppercase dark:text-gray-400"
-                        key={item.id}
-                      >
-                        {item.libelle}
-                      </th>
-                    ))}
-                  </tr>
-                </thead>
-              </table>
-            </div>
-          </article>
-          <article className="flex flex-col shadow my-4">
-            <a href="#" className="hover:opacity-75"></a>
-            <div className="bg-white flex flex-col justify-start p-6">
-              <a
-                href="#"
-                className="text-blue-700 text-sm font-bold uppercase pb-4"
-              >
-                Clés
-              </a>
-              <table className="min-w-full divide-y divide-gray-200 table-fixed dark:divide-gray-700">
-                <thead className="bg-gray-100 dark:bg-gray-700">
-                  <tr>
-                    {datahead.map((item) => (
-                      <th
-                        scope="col"
-                        className="py-3 px-6 text-xs font-medium tracking-wider text-center text-gray-700 uppercase dark:text-gray-400"
-                        key={item.id}
-                      >
-                        {item.libelle}
-                      </th>
-                    ))}
-                  </tr>
-                </thead>
-              </table>
-            </div>
-          </article>
-          <article className="flex flex-col shadow my-4">
-            <a href="#" className="hover:opacity-75"></a>
-            <div className="bg-white flex flex-col justify-start p-6">
-              <a
-                href="#"
-                className="text-blue-700 text-sm font-bold uppercase pb-4"
-              >
-                Accessoires
-              </a>
-              <table className="min-w-full divide-y divide-gray-200 table-fixed dark:divide-gray-700">
-                <thead className="bg-gray-100 dark:bg-gray-700">
-                  <tr>
-                    {datahead.map((item) => (
-                      <th
-                        scope="col"
-                        className="py-3 px-6 text-xs font-medium tracking-wider text-center text-gray-700 uppercase dark:text-gray-400"
-                        key={item.id}
-                      >
-                        {item.libelle}
-                      </th>
-                    ))}
-                  </tr>
-                </thead>
-              </table>
-            </div>
-          </article>
-          <article className="flex flex-col shadow my-4">
-            <a href="#" className="hover:opacity-75"></a>
-            <div className="bg-white flex flex-col justify-start p-6">
-              <a
-                href="#"
-                className="text-blue-700 text-sm font-bold uppercase pb-4"
-              >
-                Équipements
-              </a>
-              <table className="min-w-full divide-y divide-gray-200 table-fixed dark:divide-gray-700">
-                <thead className="bg-gray-100 dark:bg-gray-700">
-                  <tr>
-                    {datahead.map((item) => (
-                      <th
-                        scope="col"
-                        className="py-3 px-6 text-xs font-medium tracking-wider text-center text-gray-700 uppercase dark:text-gray-400"
-                        key={item.id}
-                      >
-                        {item.libelle}
-                      </th>
-                    ))}
-                  </tr>
-                </thead>
-              </table>
-            </div>
-          </article>
-          <article className="flex flex-col shadow my-4">
-            <a href="#" className="hover:opacity-75"></a>
-            <div className="bg-white flex flex-col justify-start p-6">
-              <a
-                href="#"
-                className="text-blue-700 text-sm font-bold uppercase pb-4"
-              >
-                Fournitures
-              </a>
-              <table className="min-w-full divide-y divide-gray-200 table-fixed dark:divide-gray-700">
-                <thead className="bg-gray-100 dark:bg-gray-700">
-                  <tr>
-                    {datahead.map((item) => (
-                      <th
-                        scope="col"
-                        className="py-3 px-6 text-xs font-medium tracking-wider text-center text-gray-700 uppercase dark:text-gray-400"
-                        key={item.id}
-                      >
-                        {item.libelle}
-                      </th>
-                    ))}
-                  </tr>
-                </thead>
-              </table>
-            </div>
-          </article>
-          <article className="flex flex-col shadow my-4">
-            <a href="#" className="hover:opacity-75"></a>
-            <div className="bg-white flex flex-col justify-start p-6">
-              <a
-                href="#"
-                className="text-blue-700 text-sm font-bold uppercase pb-4"
-              >
-                Autres
-              </a>
-              <table className="min-w-full divide-y divide-gray-200 table-fixed dark:divide-gray-700">
-                <thead className="bg-gray-100 dark:bg-gray-700">
-                  <tr>
-                    {datahead.map((item) => (
-                      <th
-                        scope="col"
-                        className="py-3 px-6 text-xs font-medium tracking-wider text-center text-gray-700 uppercase dark:text-gray-400"
-                        key={item.id}
-                      >
-                        {item.libelle}
-                      </th>
-                    ))}
-                  </tr>
-                </thead>
-              </table>
-            </div>
-          </article>
-        </section>
+        {datafirstarticle.map((item) => (
+          <section className="w-full mx-auto md:w-2/5 px-3">
+            <article className="flex flex-col shadow my-4" key={item.id}>
+              <a href="#" className="hover:opacity-75"></a>
+              <div className="bg-white flex flex-col justify-start p-6">
+                <img src={item.image} alt={item.title} />
+                <a
+                  href="#"
+                  className="text-blue-700 text-sm text-center font-bold uppercase pb-4"
+                >
+                  {item.title}
+                </a>
+                {/* <table className="min-w-full divide-y divide-gray-200 table-fixed dark:divide-gray-700">
+                  <thead className="bg-gray-100 dark:bg-gray-700">
+                    <tr>
+                      {datahead.map((item) => (
+                        <th
+                          scope="col"
+                          className="py-3 px-6 text-xs font-medium tracking-wider text-center text-gray-700 uppercase dark:text-gray-400"
+                          key={item.id}
+                        >
+                          {item.libelle}
+                        </th>
+                      ))}
+                    </tr>
+                  </thead>
+                </table> */}
+              </div>
+            </article>
+          </section>
+        ))}
       </div>
+
+      <footer class="w-full border-t bg-white pb-12">
+        <div
+          class="relative w-full flex items-center invisible md:visible md:pb-12"
+          x-data="getCarouselData()"
+        >
+          <button
+            class="absolute bg-blue-800 hover:bg-blue-700 text-white text-2xl font-bold hover:shadow rounded-full w-16 h-16 ml-12"
+            x-on:click="decrement()"
+          >
+            &#8592;
+          </button>
+          <button
+            class="absolute right-0 bg-blue-800 hover:bg-blue-700 text-white text-2xl font-bold hover:shadow rounded-full w-16 h-16 mr-12"
+            x-on:click="increment()"
+          >
+            &#8594;
+          </button>
+        </div>
+        <div class="w-full container mx-auto flex flex-col items-center">
+          <div class="flex flex-col md:flex-row text-center md:text-left md:justify-between py-6">
+            <a href="#" class="uppercase px-3">
+              About Us
+            </a>
+            <a href="#" class="uppercase px-3">
+              Privacy Policy
+            </a>
+            <a href="#" class="uppercase px-3">
+              Terms & Conditions
+            </a>
+            <a href="#" class="uppercase px-3">
+              Contact Us
+            </a>
+          </div>
+          <div class="uppercase pb-6">&copy; myblog.com</div>
+        </div>
+      </footer>
     </>
   );
 }
---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <>
+      <nav className="w-full py-4 bg-blue-800 shadow">
+        <div className="w-full container mx-auto flex flex-wrap items-center justify-between">
+          <nav>
+            <ul className="flex items-center justify-between font-bold text-sm text-white uppercase no-underline">
+              <li>
+                <a className="pr-6" href="#">
+                  <i className="fas fa-home"></i>
+                </a>
+              </li>
+              <li>
+                <a
+                  className="hover:text-gray-200 hover:underline px-4"
+                  href="#"
+                >
+                  Entrée
+                </a>
+              </li>
+              <li>
+                <a
+                  className="hover:text-gray-200 hover:underline px-4"
+                  href="#"
+                >
+                  Sortie
+                </a>
+              </li>
+            </ul>
+          </nav>
+          <div className="flex items-center text-lg no-underline text-white pr-6">
+            <a className="" href="#">
+              <i className="fab fa-facebook"></i>
+            </a>
+            <a className="pl-6" href="#">
+              <i className="fab fa-instagram"></i>
+            </a>
+            <a className="pl-6" href="#">
+              <i className="fab fa-twitter"></i>
+            </a>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+}
+
+export default Navbar;
